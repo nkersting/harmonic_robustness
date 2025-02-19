af197dc14fac7c52fab85b3aaec4a13d51194d75
Lord Nicholas Stephen Kersting <lordkersting@Lords-Air.attlocal.net>
2025-02-18 20:44:46 -0600

    basic controls added

--- harmonic_tester/app.py
+++ harmonic_tester/app.py
@@ -10,7 +10,23 @@ app = Flask(__name__)
 def index():
     return render_template('harmonic.html')
 
+@app.route('/process_parameters', methods=['POST'])
+def process_parameters():
+    data = request.get_json()
+    n_estimators = data.get('n_estimators')
+    learning_rate = data.get('learning_rate')
+    max_depth = data.get('max_depth')
+    min_samples_split = data.get('min_samples_split')
 
+    # Do something with the parameters
+    result = {
+        'n_estimators': n_estimators,
+        'learning_rate': learning_rate,
+        'max_depth': max_depth,
+        'min_samples_split': min_samples_split
+    }
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
